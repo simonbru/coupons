3ef0906855bde4ab13528f632e101e94aabe53a0
Simon Brulhart <simon@brulhart.me>
2017-05-04 20:57:57 +0200

    Use nearbysearch instead of textsearch
    Also commit results

--- download_places.py
+++ download_places.py
@@ -1,43 +1,87 @@
 import json
+import logging
 import os
 import time
 from urllib.parse import urlencode
 
 import requests
 
+logging.basicConfig(level=logging.INFO)
 
-ENDPOINT = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+ENDPOINT = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 API_KEY = os.environ['API_KEY']
 
+LOCATIONS = [
+    "46.343135602601954,6.383056640625",
+    "46.21024960018722,7.3333740234375",
+    "46.23305294479828,8.0859375",
+    "46.18363372751014,8.909912109375",
+    "46.494610770689384,9.7283935546875",
+    "46.773730730079386,10.01953125",
+    "47.431803338643334,9.327392578125",
+    "46.87145819560722,9.140625",
+    "46.777492763768265,8.26171875",
+    "47.44666502261753,8.3935546875",
+    "47.27922900257082,7.4542236328125",
+    "46.73233101286786,7.2344970703125",
+    "46.81133924039194,6.932373046875"
+]
 
-def main():
+
+def search_results(params):
     base_params = {
         'key': API_KEY
     }
     initial_params = {
         'type': 'restaurant',
-        'query': 'Switzerland+McDonalds',
+        'keyword': 'McDonalds',
+        # 'rankby': 'distance',
+        'radius': '50000',
+        **params
     }
+
     next_params = {
         **base_params,
         **initial_params,
     }
     results = []
-    while True:
+    while range(10):  # Should never go further than 3 pages
         url = f'{ENDPOINT}?{urlencode(next_params)}'
         r = requests.get(url)
         data = r.json()
+        if data['status'] == 'INVALID_REQUEST':
+            logging.info('Page not ready, retrying...')
+            time.sleep(2)
+            continue
         results.extend(data['results'])
+
         next_page_token = data.get('next_page_token')
         if next_page_token:
             next_params = {
                 **base_params,
                 'pagetoken': next_page_token
             }
-            time.sleep(5)
+            time.sleep(2)
         else:
             break
-    print(json.dumps(results))
+    return results
+
+
+def gather_places():
+    places = {}
+    for location in LOCATIONS:
+        logging.info('Retrieving for location: %s', location)
+        for place in search_results({'location': location}):
+            key = place['place_id']
+            places[key] = place
+        logging.info('Number of places: %d', len(places))
+    return places
+
+
+def main():
+    places = gather_places()
+    print(json.dumps(places))
+
 
 if __name__ == '__main__':
     main()
