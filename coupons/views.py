847390148bb128c55831ea9101f02f7aabdf6f9d
Simon Brulhart <simon@brulhart.me>
2017-07-15 21:22:16 +0200

    Add form to notify if a coupon is working

--- coupons/views.py
+++ coupons/views.py
@@ -1,5 +1,7 @@
 import barcode
+from django.contrib import messages
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import (
     TemplateView,
@@ -7,8 +9,10 @@ from django.views.generic import (
     DetailView,
 )
 from django.views.generic.detail import SingleObjectMixin
+from ipware.ip import get_ip
 
-from coupons.models import Coupon, Category
+from .forms import CommentForm
+from .models import Coupon, Category
 
 
 class FirstView(TemplateView):
@@ -27,6 +31,32 @@ class CouponListView(ListView):
 
 class CouponDetailView(DetailView):
     model = Coupon
+
+    def get(self, request, *args, **kwargs):
+        self.comment_form = CommentForm()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.comment_form = CommentForm(data=request.POST)
+        if self.comment_form.is_valid():
+            coupon = self.get_object()
+            comment = self.comment_form.instance
+            comment.coupon = coupon
+            comment.author_ip = get_ip(request)
+            comment.save()
+            messages.success(
+                request,
+                "Votre commentaire a bien été pris en compte."
+            )
+            return redirect('coupon_detail', self.get_object().id)
+        else:
+            return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            'comment_form': self.comment_form,
+        }
 
 
 class CouponBarcodeView(SingleObjectMixin, View):
