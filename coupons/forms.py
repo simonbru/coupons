8a70390561c546e9e826ca3a02d94fc0ba720162
Simon Brulhart <simon@brulhart.me>
2017-07-18 13:54:59 +0200

    Use a different approach for custom radio inputs

--- coupons/forms.py
+++ coupons/forms.py
@@ -3,13 +3,34 @@ from django import forms
 from .models import Comment, Restaurant
 
 
+class RadioButtonSelect(forms.RadioSelect):
+    option_template_name = 'widgets/radio_button_option.html'
+
+
+class CommentChoice(str):
+    """Hack to pass more attributes to each radio option
+
+    Since we can only pass a value and a label for each choice,
+    we set additional attributes on the label string instead.
+    """
+    def __new__(cls, value, css_classes='btn-primary', bs_icon=None):
+        string = super().__new__(cls, value)
+        string.css_classes = css_classes
+        string.bs_icon = bs_icon
+        return string
+
+
 class CommentForm(forms.ModelForm):
     does_coupon_work = forms.ChoiceField(
         choices=[
-            (True, "Fonctionne"),
-            (False, "Ne fonctionne pas"),
+            (True, CommentChoice(
+                "Fonctionne", 'btn-success', 'thumbs-up'
+            )),
+            (False, CommentChoice(
+                "Ne fonctionne pas", 'btn-danger', 'thumbs-down'
+            )),
         ],
-        widget=forms.RadioSelect,
+        widget=RadioButtonSelect,
     )
     restaurant = forms.ModelChoiceField(
         queryset=Restaurant.objects.by_distance(),
